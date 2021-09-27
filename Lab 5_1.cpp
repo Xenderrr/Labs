1c0ab431daa292f9bde616b921cee974da97d5d5
Xenderrr <90962222+Xenderrr@users.noreply.github.com>
2021-09-27 18:54:21 +0300

    Add files via upload

---
+++ Lab 5_1.cpp
@@ -0,0 +1,25 @@
+﻿
+#include <iostream>
+#include <cmath>
+
+using namespace std;
+
+int main()
+{
+    int x1, x2, y1, y2;
+    cout << "This program calculates the distance between two points" << endl;
+    cout << "Enter coordinates of point A:" << endl << "x1 = ";
+    cin >> x1;
+    cout << "y1 = ";
+    cin >> y1;
+    cout << "Enter coordinates of point B:" << endl <<"x2 = ";
+    cin >> x2;
+    cout << "y2 = ";
+    cin >> y2;
+    float dist;
+    dist = sqrt(pow(x2 - x1, 2) + pow(y2 - y1, 2));
+    cout << "Distance is " << dist << endl;
+
+    return 0;
+}
+
---
+++ Lab 5_2.cpp
@@ -0,0 +1,33 @@
+﻿#include <iostream>
+#include <cmath>
+
+using namespace std;
+
+int main()
+{
+    int A, B, C;
+    cout << "This program returns the length of sections AC and BC and their sum." << endl;
+    cout << "Enter the coordinates of the points:" << endl << "A = ";
+    cin >> A;
+    cout << "B = ";
+    cin >> B;
+    cout << "C = ";
+    cin >> C;
+    int AC, BC, sum;
+    if (A >= C) {
+        AC = A - C;
+    }
+    else {
+        AC = C - A;
+    }
+    if (B >= C) {
+        BC = B - C;
+    }
+    else {
+        BC = C - B;
+    }
+    sum = AC + BC;
+    cout << "AC = " << AC << ", BC = " << BC << ", AC + BC = " << sum;
+
+    return 0;
+}
