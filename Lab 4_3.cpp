4b2f371a871fcbdf92a7505dd95e48ce07e62bb5
Xenderrr <90962222+Xenderrr@users.noreply.github.com>
2021-10-02 12:52:06 +0300

    Update Lab 4_3.cpp

--- Lab 4_3.cpp
+++ Lab 4_3.cpp
@@ -5,7 +5,7 @@ using namespace std;
 
 int main()
 {
-    int a, b;
+    float a, b;
     cout << "This program counts arithmetical mean" << endl;
     cout << "Enter two numbers" << endl;
     cin >> a;
